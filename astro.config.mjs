4c18466903ee27614b5fab976bb1bb94b9c779fe
hudong798 <hudong798@gmail.com>
2025-11-06 16:57:08 +0800

    256

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,8 +28,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
-	site: "",
-    // site: "https://demo-firefly.netlify.app/",
+    site: "https://www.52798.xyz/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
