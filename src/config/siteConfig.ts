2fef0fac7f9ca60cfc23be16b323d71e23dc01b2
hudong798 <hudong798@gmail.com>
2026-01-09 20:16:01 +0800

    ff

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -44,7 +44,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "/assets/images/favicon.ico",
+			src: "/assets/images/favicon1.ico",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
